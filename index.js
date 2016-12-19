e20aba4159e6beee46fc35c93012c11ba17cd9f8
Jan Moeller <jan.moeller0@gmail.com>
2016-12-19 19:32:49 +0100

    Add routing

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 var koa = require('koa');
+var historyApiFallback = require('koa-history-api-fallback');
+var marko = require('marko');
+var send = require('koa-send');
+var router = require('koa-router')();
+
 var app = koa();
+var mntBlogTmpl = marko.load(require.resolve('./dist/blog/index.html'));
 
+router.get(
+    /^\/blog(?:\/|$)/,
+  historyApiFallback(),
+  function *(next) {
+    if(this.url != '/index.html') {
+      yield next;
+    } else {
+      this.type = 'text/html';
+      this.body = mntBlogTmpl.stream({
+        initial_state: JSON.stringify({test: 'hi'})
+      });
+    }
+  },
+  function *(){
+    yield send(this, this.path, { root: __dirname + '/dist' });
+  }
+);
 
-var template = require('marko').load(require.resolve('./dist/index.html'));
-app.use(function *() {
-  this.type = 'text/html';
-  this.body = template.stream({
-    initial_state: JSON.stringify({test: 'hi'})
-  });
-});
+app
+  .use(router.routes());
 
 app.listen(3000);
 
