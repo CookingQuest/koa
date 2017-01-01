bdc3b0a95f928623c7383868399c2f99ec6dc43a
Jan Moeller <jan.moeller0@gmail.com>
2017-01-01 04:52:08 +0100

    Mod port; add todo

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ var router = require('koa-router')();
 var app = koa();
 var mntBlogTmpl = marko.load(require.resolve('./dist/blog/index.html'));
 
+const port = 3001;
+
 router.get(
     /^\/blog(?:\/|$)/, 
   function *() {
     this.type = 'text/html';
     this.body = mntBlogTmpl.stream({
       initial_state: JSON.stringify({test: 'hi'})
-    }); 
+    });
   }
 );
 
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(port);
 
-console.log('listening on port 3000');
+console.log('listening on port ' + port);
