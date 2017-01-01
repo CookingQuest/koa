8b88f7ff35fe97db9665864dec392e095aa988ea
Jan Moeller <jan.moeller0@gmail.com>
2017-01-01 02:24:37 +0100

    Remove static file handling

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 var koa = require('koa');
-var historyApiFallback = require('koa-history-api-fallback');
 var marko = require('marko');
-var send = require('koa-send');
 var router = require('koa-router')();
 
 var app = koa();
 var mntBlogTmpl = marko.load(require.resolve('./dist/blog/index.html'));
 
 router.get(
-    /^\/blog(?:\/|$)/,
-  historyApiFallback(),
-  function *(next) {
-    if(this.url != '/index.html') {
-      yield next;
-    } else {
-      this.type = 'text/html';
-      this.body = mntBlogTmpl.stream({
-        initial_state: JSON.stringify({test: 'hi'})
-      });
-    }
-  },
-  function *(){
-    yield send(this, this.path, { root: __dirname + '/dist' });
+    /^\/blog(?:\/|$)/, 
+  function *() {
+    this.type = 'text/html';
+    this.body = mntBlogTmpl.stream({
+      initial_state: JSON.stringify({test: 'hi'})
+    }); 
   }
 );
 
-app
-  .use(router.routes());
+app.use(router.routes());
 
 app.listen(3000);
 
