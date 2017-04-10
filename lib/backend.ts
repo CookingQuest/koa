de8cb850c0e0e7fb9d339969eb4f67319ecf1a82
Jan Moeller <jan.moeller0@gmail.com>
2017-04-10 20:49:56 +0200

    Add authMiddleware

--- lib/backend.ts
+++ lib/backend.ts
@@ -4,20 +4,23 @@ import { Socket, Channel } from '../lib/phoenix';
 export class PhoenixBackend implements Backend {
   channel: Channel;
 
-
   async connect(): Promise<object> {
-    const socket = new Socket('ws://localhost:4000/socket', {transport: ws});
+    const socket = new Socket('ws://localhost:4000/socket', { transport: ws });
     socket.connect();
     this.channel = socket.channel('state', {});
     return new Promise((resolve, reject) => this.channel.join(1000)
-                       .receive('ok', resolve)
-                       .receive('timeout', reject));
+      .receive('ok', resolve)
+      .receive('timeout', reject));
   }
 
   async getInitialState(route: string, userHash: string): Promise<object> {
     return new Promise(resolve => this.channel.push(
-      'get', {route, user_hash: userHash}, 5000
-    ).receive('ok', ({data}) => resolve(data)));
+      'get', { route, user_hash: userHash }, 5000
+    ).receive('ok', ({ data }) => resolve(data)));
+  }
+
+  async register(token: string): Promise<boolean> {
+    return new Promise<boolean>(resolve => resolve(false));
   }
 
 }
@@ -25,4 +28,5 @@ export class PhoenixBackend implements Backend {
 export interface Backend {
   connect(): Promise<object>;
   getInitialState(route: string, userHash: string): Promise<object>;
+  register(token: string): Promise<boolean>;
 }
--- lib/index.ts
+++ lib/index.ts
@@ -2,10 +2,17 @@ import { Server } from './server';
 import { RenderMiddleware } from './renderMiddleware';
 import { ApiMiddleware } from './apiMiddleware';
 
+import { PhoenixBackend } from './backend';
+
 createServer().then((server) => server.start(3001));
 
 async function createServer() {
-  const api = await ApiMiddleware();
+  const backend = new PhoenixBackend()
+  backend.connect().catch(() => {
+    console.error('api offline');
+    process.exit()
+  });
+  const api = await ApiMiddleware(backend);
   return new Server().use([
     api, RenderMiddleware('templates/index.marko')
   ]);
