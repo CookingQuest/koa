f50b03e2817b37d6238981ef479eb1c4d0f1668f
Jan Moeller <jan.moeller0@gmail.com>
2017-04-13 20:58:24 +0200

    Remove fallback user

--- lib/apiMiddleware.ts
+++ lib/apiMiddleware.ts
@@ -5,7 +5,7 @@ import { Backend } from './backend';
 export const ApiMiddleware = (backend: Backend): Middleware => {
   return async (ctx: Context, next: () => Promise<any>): Promise<any> => {
     let user = ctx.cookies.get('user', { signed: true });
-    const initialState = await backend.getInitialState(ctx.request.url, user || '1')
+    const initialState = await backend.getInitialState(ctx.request.url, user)
     ctx.state.initialState = initialState;
     await next();
   }
